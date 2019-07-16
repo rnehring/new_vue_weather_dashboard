b50a0437592765ec97d264e080f21852c88338ef
Ryan Nehring <rnehring@PUREFLEX.COM>
2019-07-16 11:00:17 -0400

    First Commit

---
+++ src/components/content.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="position: relative;">
+      <temp-var-chart :tempVar="tempVar"></temp-var-chart>
+      <today-highlights :highlights="highlights"></today-highlights>
+  </div>
+</template>
+
+<script>
+import TempVarChart from './tempvarchart.vue';
+import Highlights from './highlights.vue';
+
+export default {
+  props: ['highlights', 'tempVar'],
+  components: {
+    'temp-var-chart': TempVarChart,
+    'today-highlights': Highlights
+  }
+}
+</script>
+ 
---
+++ src/components/highlights.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="custom-content-card content-card card">
+    <div class="card-body pb-0">
+    <div class="content-header h4 text-center pt-2 pb-3">Highlights</div>
+      <div class="row">
+        <uv-index :highlights="highlights"></uv-index>
+        <visibility :highlights="highlights"></visibility>
+        <wind-status :highlights="highlights"></wind-status>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import UVIndex from "./uvindex.vue";
+import Visibility from "./visibility.vue";
+import WindStatus from "./windStatus.vue";
+
+export default {
+  props: ["highlights"],
+  components: {
+    "uv-index": UVIndex,
+    "visibility": Visibility,
+    "wind-status": WindStatus,
+  }
+};
+</script>
+ 
